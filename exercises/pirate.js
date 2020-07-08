e62adf0de19b2d843722d64f27bd45c16d58534c
Justyna <justyna.field@gmail.com>
2020-07-08 13:25:00 -0600

    Pirate creature

---
+++ exercises/pirate.js
@@ -0,0 +1,21 @@
+class Pirate {
+  constructor (name, job) {
+    this.name = name;
+    if (job === undefined) {
+      this.job = 'scallywag';
+    } else {
+      this.job = job;
+    };
+    this.cursed = false;
+    this.booty = 0;
+  }
+
+  robShip () {
+    this.booty +=100;
+    return 'YAARRR!'
+  }
+
+}
+
+
+module.exports = Pirate;
