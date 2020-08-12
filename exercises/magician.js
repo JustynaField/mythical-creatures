6ae0d1874412d623e242e16cfc6d133ee146432e
Justyna <justyna.field@gmail.com>
2020-08-12 15:19:05 -0600

    Magician creature

--- exercises/magician.js
+++ exercises/magician.js
@@ -1,10 +1,24 @@
 class Magician {
-  constructor ({ name }={}) {
+  constructor ({name, topHat} = {}) {
     this.name = name;
+    this.topHat = topHat === undefined ? true : false;
+    this.confident = false;
+    this.counter = 0;
+  }
+
+  incantation (text) {
+    return text.toUpperCase() + "!"
+  }
+  cast () {
+    this.counter ++;
+    if (this.counter === 3) {
+      this.confident = true;
+    }
+    return !this.topHat ? 'PULL DOVE FROM SLEEVE' : 'PULL RABBIT FROM TOP HAT'
+  }
+  performShowStopper () {
+    return this.confident ? 'WOW! The magician totally just sawed that person in half!' : 'Oh no! Practice more before attempting this trick!'
   }
 }
-
-
-
 
 module.exports = Magician;
